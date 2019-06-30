d51b4e121239d6557dcd45f8839d27dfc1f61ad4
FoxyelperroDJ <39922495+FoxyelperroDJ@users.noreply.github.com>
2019-06-30 04:54:25 -0300

    New Update ;3
    With Titles Is Better ❤

--- src/Lobby/tasks/NewLobbyTask.php
+++ src/Lobby/tasks/NewLobbyTask.php
@@ -27,28 +27,28 @@ class NewLobbyTask extends PluginTask{
 
 		if($this->seconds == 1){
 			$this->plugin->Particle($player);
-			$player->sendMessage(C::GREEN . "Teleporting in 5 seconds");
+			$player->addTitle(C::GREEN . "Teleporting in 5 seconds");
 		}
 
 		if($this->seconds == 2){
 			$this->plugin->Particle($player);
-			$player->sendMessage(C::GREEN . "Teleporting in 4 seconds");
+			$player->addTitle(C::GREEN . "Teleporting in 4 seconds");
 		}
 
 		if($this->seconds == 3){
 			$this->plugin->Particle($player);
-			$player->sendMessage(C::GREEN . "Teleporting in 3 seconds");
+			$player->addTitle(C::GREEN . "Teleporting in 3 seconds");
 		}
 
 		if($this->seconds == 4){
 			$this->plugin->Particle($player);
-			$player->sendMessage(C::GREEN . "Teleporting in 2 seconds");
+			$player->addTitle(C::GREEN . "Teleporting in 2 seconds");
 		}
 
 		if($this->seconds == 5){
 			$this->plugin->Particle($player);
-			$player->sendMessage(C::GREEN . "Teleporting in 1 seconds");
-			$player->sendMessage(C::GREEN . "Teleporting now....");
+			$player->addTitle(C::GREEN . "Teleporting in 1 seconds");
+			$player->addTitle(C::GREEN . "Teleporting now....");
 			$player->teleport($level);
 			$this->plugin->stopTask($this->getTaskId(), "3.0.0-ALPHA12");
 		}
